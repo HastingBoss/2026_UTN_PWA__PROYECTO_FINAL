8b5d02b0ff7c2bac1d51f8a4b4a5b6abbf70dc92
Hasting.dev <lukguz99@gmail.com>
2026-05-28 20:42:41 -0300

    2

--- Backend/src/config/mongodb.config.js
+++ Backend/src/config/mongodb.config.js
@@ -1,17 +1,19 @@
 import mongoose from "mongoose"
 import ENVIRONMENT from "./environment.config.js"
 
-const connectMongoDB = async ( ) => {
-    try{
+const connectMongoDB = async () => {
+    try {
         await mongoose.connect(
-            ENVIRONMENT.MONGO_DB_CONNECTION_STRING + '/' + ENVIRONMENT.MONGO_DB_NAME
+            ENVIRONMENT.MONGO_DB_CONNECTION_STRING + '/' + ENVIRONMENT.MONGO_DB_NAME, {
+            connectTimeoutMS: 30000
+        }
         )
         console.log("La conexion con MongoDB funciona")
     }
-    catch(error){
+    catch (error) {
         console.error("Hubo un fallo en la conexion de la DB", error)
     }
-    
+
 }
 
 export default connectMongoDB
